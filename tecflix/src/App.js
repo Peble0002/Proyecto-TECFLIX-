8974ee9de91b4166e0664803752a5a9f5fca5f48
Pablo Chaves <pablo290418@gmail.com>
2022-08-17 00:08:44 -0600

    varios

--- tecflix/src/App.js
+++ tecflix/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState, useEffect } from 'react';
+import Navbar from './Components/Navbar';
+import listaVideos from './Components/listaVideos';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function  App(){
+
+  const [video, setVideo] = useState({
+    album: '',
+    titulo: '',
+    emisor: '',
+    duracion: 0,
+    enlace: ''
+  })
+
+  const [videos, setVideos] = useState([])
+  const [listUpdated, setListUpdated] = useState(false)
+
+  useEffect(() => {
+      const getVideo=()=>{
+        fetch('http://localhost:8080/api')
+        .then(res => res.json())
+        .then(res => setVideos(res))
+      }
+      getVideo();
+      setListUpdated(false)
+    }, [listUpdated])
+  
+
+    return (
+      <Fragment>
+     <Navbar brand='Canciones'/>
+     <div className="container">
+        <div className="row">
+          <div className="col-7">
+            <h2 style={{textAlign: 'center'}}>Lista de Videos</h2>
+            <listaVideos video={video} setVideos={setVideos} videos={videos} setListUpdated={setListUpdated}/>
+          </div>
+          <div className="col-5">
+            <h2 style={{textAlign: 'center'}}>Agregar Video</h2>
+            <Form video={video} setVideo={setVideo}/>
+          </div>
+        </div>
+      </div>
+      </Fragment>
+      
+    );
+
+
 }
 
 export default App;
---
+++ tecflix/src/Components/listaVideos.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+
+const listaVideos = () => {
+    return (
+        <table className="table">
+        <thead>
+            <tr>
+                <th>Album</th>
+                <th>Titulo</th>
+                <th>Emisor</th>
+                <th>Duracion</th>
+                <th>Enlace</th>
+            </tr>
+        </thead>
+        <tbody>
+        <th>Album</th>
+        <th>Album</th>
+                <th>Titulo</th>
+                <th>Emisor</th>
+                <th>Duracion</th>
+                <th>Enlace</th>
+        </tbody>
+    </table>
+    );
+}
+
+export default listaVideos;
