1684643abd7929ed7f86538dfb70dcac4d0c81d2
Pablo Chaves <pablo290418@gmail.com>
2022-08-18 14:54:03 -0600

    .

--- tecflix/src/App.js
+++ tecflix/src/App.js
@@ -2,18 +2,10 @@ import React, { Fragment, useState, useEffect } from 'react';
 import Navbar from './Components/Navbar';
 import listaVideos from './Components/listaVideos';
 
+
 function  App(){
 
-  const [video, setVideo] = useState({
-    album: '',
-    titulo: '',
-    emisor: '',
-    duracion: 0,
-    enlace: ''
-  })
-
   const [videos, setVideos] = useState([])
-  const [listUpdated, setListUpdated] = useState(false)
 
   useEffect(() => {
       const getVideo=()=>{
@@ -22,8 +14,7 @@ function  App(){
         .then(res => setVideos(res))
       }
       getVideo();
-      setListUpdated(false)
-    }, [listUpdated])
+    }, [])
   
 
     return (
@@ -33,11 +24,13 @@ function  App(){
         <div className="row">
           <div className="col-7">
             <h2 style={{textAlign: 'center'}}>Lista de Videos</h2>
-            <listaVideos video={video} setVideos={setVideos} videos={videos} setListUpdated={setListUpdated}/>
+            <listaVideos videos={videos}/>
+            
           </div>
           <div className="col-5">
             <h2 style={{textAlign: 'center'}}>Agregar Video</h2>
-            <Form video={video} setVideo={setVideo}/>
+            
+            
           </div>
         </div>
       </div>
@@ -45,7 +38,7 @@ function  App(){
       
     );
 
-
+//<video={video} setVideo={setVideo}/>
 }
 
 export default App;
--- tecflix/src/Components/listaVideos.js
+++ tecflix/src/Components/listaVideos.js
@@ -1,27 +1,30 @@
 import React from "react";
 
 
-const listaVideos = () => {
+const listaVideos = ({videos}) => {
     return (
         <table className="table">
-        <thead>
-            <tr>
-                <th>Album</th>
-                <th>Titulo</th>
-                <th>Emisor</th>
-                <th>Duracion</th>
-                <th>Enlace</th>
-            </tr>
-        </thead>
-        <tbody>
-        <th>Album</th>
-        <th>Album</th>
-                <th>Titulo</th>
-                <th>Emisor</th>
-                <th>Duracion</th>
-                <th>Enlace</th>
-        </tbody>
-    </table>
+            <thead>
+                <tr>
+                    <th>Album</th>
+                    <th>Titulo</th>
+                    <th>Emisor</th>
+                    <th>Duracion</th>
+                    <th>Enlace</th>
+                </tr>
+            </thead>
+            <tbody>
+                {videos.map(video=>(
+                <tr key={video.id}>
+                    <th>{video.Album}</th>
+                    <th>{video.Titulo}</th>
+                    <th>{video.Emisor}</th>
+                    <th>{video.Duracion}</th>
+                    <th>{video.Enlace}</th>
+                </tr>
+               ))}
+            </tbody>
+        </table>
     );
 }
 
